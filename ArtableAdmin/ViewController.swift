aab5e5fa1544bedf8d406c927380ef8e48f0eff1
franklin <franklinlivemail@gmail.com>
2021-01-23 21:32:13 +0100

    102

--- ArtableAdmin/ViewController.swift
+++ ArtableAdmin/ViewController.swift
@@ -8,12 +8,11 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+poban
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
 
 }
 
